59f39815a8603ccbff53f17490dc72c440a02c8c
wangyongqi <wangyongqi@baidu.com>
2020-11-19 16:47:48 +0800

    添加登录态

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
 import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Raffle from './components/Raffle/Raffle.js';
+import {UserInfoContext} from './components/context';
+
 
 function App() {
+    const [userInfo, setUserInfo] = useState({});
+
+    // 这个fetch函数可以用useMero保存起来，或者从外部导入
+    function fetchUserInfo() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    userName: 'Tom',
+                    loggin: true
+                });
+            }, 1000);
+        });
+    }
+
+    useEffect(() => {
+        fetchUserInfo().then(userInfo => {
+            setUserInfo(userInfo);
+        });
+    }, []);
+
+    const onLoginChange = () => {
+        if (userInfo.loggin) {
+            setUserInfo({});
+            return;
+        }
+        fetchUserInfo().then(userInfo => {
+            setUserInfo(userInfo);
+        });
+    };
+
   return (
     <BrowserRouter>
-      <Link to="/home">首页</Link>
-      <br/>
-      <Link to="/about">关于</Link>
-       <br/>
-      <Link to="/raffle">抽奖</Link>
+        <UserInfoContext.Provider value={userInfo}>
+            <Link to="/home">首页</Link>
+            <br/>
+            <Link to="/about">关于</Link>
+            <br/>
+            <Link to="/raffle">抽奖</Link>
 
+            <input type="button" onClick={onLoginChange} value={userInfo.loggin ? '退出' : '登录'}></input>
 
-
-      <Route path="/home" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/raffle" component={Raffle}/>
-  
+            <Route path="/home" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/raffle" component={Raffle}/>
+        </UserInfoContext.Provider>
     </BrowserRouter>
   )
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import {UserInfoContext} from '../context';
 
 class Home extends Component {
+
+    static contextType = UserInfoContext;
     
-    render() { 
+    render() {
+        const {userName, loggin} = this.context;
         return (
-           <p>关于页面</p>
-        )
+            <>
+                <p>关于页面</p>
+                <p>userName: {userName} </p>
+                <p>登陆状态: {loggin ? '已登录' : '未登录'}</p>
+            </>
+        );
     }
 }
  
