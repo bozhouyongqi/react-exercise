2364773346159afb7673395ca3698efa0a430243
须学 <wangyongqi.wyq@antgroup.com>
2021-12-26 13:00:56 +0800

    安装body-pix,tensorflow/tfjs

--- src/components/PictureSegmentation/PictureSegmentation.js
+++ src/components/PictureSegmentation/PictureSegmentation.js
@@ -1,10 +1,45 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 import './PictureSegmentation.css';
 
+const bodyPix = require('@tensorflow-models/body-pix');
 
 export default function PictureSegmentation() {
 
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    async function segment() {
+      console.log('use effect', bodyPix)
+      const net = await bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 0.75,
+        quantBytes: 2
+      })
+      .catch(error => {
+        console.log('load engine error', error)
+      })
+      /**
+       * One of (see documentation below):
+       *   - net.segmentPerson
+       *   - net.segmentPersonParts
+       *   - net.segmentMultiPerson
+       *   - net.segmentMultiPersonParts
+       * See documentation below for details on each method.
+        */
+      console.log('imageRef.current ', imageRef.current)
+      const segmentation = await net.segmentPerson(imageRef.current)
+        .catch((error) => {
+          console.log('error', error)
+        })
+      console.log(segmentation);
+    }
+    segment();
+
+    return () => {};
+
+  }, [imageRef])
 
 
   return (
@@ -12,7 +47,7 @@ export default function PictureSegmentation() {
       <div>
         人体分割
       </div>
-      <img src="/fbb2.jpeg" width="300px"></img>
+      <img src="/fbb2.jpeg" width="300px" ref={imageRef}></img>
     </>
   );
 }
