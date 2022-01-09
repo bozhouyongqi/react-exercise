23d48146bf2dd94c4eb4996d692dafc02b87c083
须学 <wangyongqi.wyq@antgroup.com>
2022-01-09 18:21:22 +0800

    删除多余路由

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
-import Home from './components/Home/Home.js';
-import About from './components/About/About.js';
-import Raffle from './components/Raffle/Raffle.js';
 import {UserInfoContext} from './components/context';
-import TestPage from './components/Test/test.js';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 import PictureSegmentation from './components/PictureSegmentation/PictureSegmentation'
 
@@ -45,14 +41,6 @@ function App() {
   return (
     <BrowserRouter>
         <UserInfoContext.Provider value={userInfo}>
-            <Link to="/home">首页</Link>
-            <br/>
-            <Link to="/about">关于</Link>
-            <br/>
-            <Link to="/raffle">抽奖</Link>
-            <br/>
-            <Link to="/test">测试页</Link>
-            <br/>
             <Link to="/videoPlayer">视频播放器</Link>
             <br/>
             <Link to="/pictureSegmentation">人体分割</Link>
@@ -61,10 +49,6 @@ function App() {
 
             <input type="button" onClick={onLoginChange} value={userInfo.loggin ? '退出' : '登录'}></input>
 
-            <Route path="/home" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/raffle" component={Raffle}/>
-            <Route path='/test' component={TestPage}/>
             <Route path='/videoPlayer' component={VideoPlayer}/>
             <Route path='/pictureSegmentation' component={PictureSegmentation}/>
         </UserInfoContext.Provider>
