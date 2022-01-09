5a9ce4a7b775a9f763a53ed8ad1cfa63d72473a1
须学 <wangyongqi.wyq@antgroup.com>
2022-01-09 23:40:48 +0800

    修改servePath

---
+++ docs/precache-manifest.58a44fd5489f6c5e880a349b98ed627b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e7699ae98a606313b1bc5cff4504c",
+    "url": "/Users/wangyongqi/ant/learn/react-exerciseindex.html"
+  },
+  {
+    "revision": "32b3418763f55c0683c5",
+    "url": "/Users/wangyongqi/ant/learn/react-exercisestatic/css/main.5e785cf1.chunk.css"
+  },
+  {
+    "revision": "cafbc08ab11f06600d40",
+    "url": "/Users/wangyongqi/ant/learn/react-exercisestatic/js/2.a4ac542b.chunk.js"
+  },
+  {
+    "revision": "32b3418763f55c0683c5",
+    "url": "/Users/wangyongqi/ant/learn/react-exercisestatic/js/main.798ac6af.chunk.js"
+  },
+  {
+    "revision": "d9c197f092aca22c2d6f",
+    "url": "/Users/wangyongqi/ant/learn/react-exercisestatic/js/runtime-main.2dd5f558.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Users/wangyongqi/ant/learn/react-exercisestatic/media/logo.25bf045c.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d87b004aea2561fb09918645d260186.js"
+  "/Users/wangyongqi/ant/learn/react-exerciseprecache-manifest.58a44fd5489f6c5e880a349b98ed627b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Users/wangyongqi/ant/learn/react-exercis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
