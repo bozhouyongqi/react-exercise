e6874ca058f10a956be876ca650abff673b0aaa8
须学 <wangyongqi.wyq@antgroup.com>
2022-01-09 23:26:32 +0800

    修改编译目录为docs

---
+++ docs/precache-manifest.6d87b004aea2561fb09918645d260186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca6f1ca160fefd597faaa91c5a7b220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c2676f1d9e82ed910a",
+    "url": "/static/css/main.5e785cf1.chunk.css"
+  },
+  {
+    "revision": "cafbc08ab11f06600d40",
+    "url": "/static/js/2.a4ac542b.chunk.js"
+  },
+  {
+    "revision": "88c2676f1d9e82ed910a",
+    "url": "/static/js/main.26b19539.chunk.js"
+  },
+  {
+    "revision": "88ed9670f57107c7dc97",
+    "url": "/static/js/runtime-main.ce50c451.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
