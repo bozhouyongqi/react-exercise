9d5f419090dade588cb5946452fb3c805d47da74
须学 <wangyongqi.wyq@antgroup.com>
2022-01-10 00:14:36 +0800

    url-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -368,7 +368,7 @@ module.exports = function(webpackEnv) {
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: '[name].[hash:8].[ext]',
               },
             },
             // Process application JS with Babel.
